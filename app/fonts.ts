8c7403cd1439e52b2105db92ac6fad400fa6b092
andresengler <141840530+andresengler@users.noreply.github.com>
2025-06-17 15:25:57 -0300

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,20 +1,10 @@
-
 import localFont from 'next/font/local'
 
+export const ppNeueMontrealMono = localFont({
   src: '../public/fonts/PPNeueMontrealMono-Regular.woff2',
   weight: '400',
+  variable: '--font-ppneue-mono',
 })
-
-  src: '../public/fonts/PPNeueMontreal-Medium.woff2',
-  weight: '500',
-})
-
-export const departureMono = localFont({
-  src: '../public/fonts/DepartureMono-Regular.woff2',
-  weight: '400',
-  variable: '--font-departure-mono',
-})
-
 
 export const ppNeueMontrealMedium = localFont({
   src: '../public/fonts/PPNeueMontreal-Medium.woff2',
@@ -22,9 +12,14 @@ export const ppNeueMontrealMedium = localFont({
   variable: '--font-ppneue-medium',
 })
 
-
 export const ppNeueMontrealRegular = localFont({
   src: '../public/fonts/PPNeueMontreal-Regular.woff2',
   weight: '400',
   variable: '--font-ppneue-regular',
+})
+
+export const departureMono = localFont({
+  src: '../public/fonts/DepartureMono-Regular.woff2',
+  weight: '400',
+  variable: '--font-departure-mono',
 })
