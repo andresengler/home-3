922f4370778732da6c40d3eb9204c1662b260660
andresengler <141840530+andresengler@users.noreply.github.com>
2025-06-17 15:53:24 -0300

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,8 @@
-"use client"
+'use client'
 
 import { useState, useEffect } from "react"
 import { BraunClock } from "./BraunClock"
-
+import { departureMono } from "@/app/fonts"
 
 export function Footer() {
   const [time, setTime] = useState(new Date())
@@ -20,7 +20,7 @@ export function Footer() {
       <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="border-t border-gray-100 dark:border-gray-800 py-4 flex justify-between items-center">
           <p
-            className={` text-[10px] sm:text-[12px] font-normal tracking-normal text-gray-400 dark:text-gray-500`}
+            className={`${departureMono.variable} text-[10px] sm:text-[12px] font-normal tracking-normal text-gray-400 dark:text-gray-500`}
           >
             Keep your mind thinking about whatever is true.
           </p>
@@ -32,4 +32,3 @@ export function Footer() {
     </footer>
   )
 }
-
